c93d4c02126efd8ec6ff76acb9d95248872f0fa3
mvaradi <mvaradi@ebi.ac.uk>
2021-03-01 14:32:38 +0000

    Improved comments

--- src/app/multi-view/multi-view.models.ts
+++ src/app/multi-view/multi-view.models.ts
@@ -1,25 +1,25 @@
 export class UniProtMapping {
-  accession: string;
-  name: string;
-  identifier: string;
+  accession: string; // UniProt accession of PRO identifier
+  name: string; // Name of the protein
+  identifier: string; // Identifier of the protein
   mappings: Mapping[];
-  description: string;
+  description: string; // Description of the protein
 }
 
 class Mapping {
-  entity_id: number;
-  chain_id: string;
-  struct_asym_id: string;
+  entity_id: number; // PDB entity identifier
+  chain_id: string; // PDB chain identifier
+  struct_asym_id: string; // PDB asymmetric unit identifier
   start: Range;
   end: Range;
-  unp_start: number;
-  unp_end: number;
-  pdb_start: number;
-  pdb_end: number;
+  unp_start: number; // First UniProt residue index
+  unp_end: number; // Last UniProt residue index
+  pdb_start: number; // First PDB residue index
+  pdb_end: number; // Last PDB residue index
 }
 
 class Range {
-  author_residue_number: number;
-  author_insertion_code: string;
-  residue_number: number;
+  author_residue_number: number; // PDB author residue number
+  author_insertion_code: string; // PDB author insertion code
+  residue_number: number; // PDB residue number
 }
